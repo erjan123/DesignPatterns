524f82c42e08246a773ec7f41e528ad411989836
erjan123 <erjan123@yahoo.com>
2018-11-26 11:57:16 +0200

    Created Projects
    Created Projects

---
+++ Behavioral Patterns/Chain of Resp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Chain_of_Resp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Creational Patterns/Factory Method/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Factory_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
