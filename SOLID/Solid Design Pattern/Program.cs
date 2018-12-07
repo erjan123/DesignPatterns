dd9f9afbd93695fd6a39964065c19c48964de980
erjan123 <erjan123@yahoo.com>
2018-12-07 21:34:40 +0200

    Added Solid Patterns project

---
+++ SOLID/Solid Design Pattern/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Solid_Design_Pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
