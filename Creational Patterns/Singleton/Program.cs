0410f19312e741e24edc57386adc1f3d619a7d9d
erjan123 <erjan123@yahoo.com>
2018-12-12 17:22:08 +0200

    Singleton with static instance

---
+++ Creational Patterns/Singleton/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Logger1 a = new Logger1(); - cannot create a new Logger
+            Logger1.Instance.LogMessage("Please Log this message!");
+            Logger1.Instance.LogMessage("Please Log this message, too!");
+
+            Console.WriteLine();
+            // Logger2 a = new Logger2; - cannot create a new Logger2
+            // GetInstance() method returns the only single Logger2 instance
+            Logger2.GetInstance().LogMessage("Can you Log this message?");
+            Logger2.GetInstance().LogMessage("Can you Log this message, too?");
+
+            Console.Read();
+        }
+    }
+}
--- Creational Patterns/Singleton/Singleton1.cs
+++ Creational Patterns/Singleton/Singleton1.cs
@@ -2,11 +2,44 @@
 
 namespace Singleton
 {
-    public static class Singleton
+    public class Logger1
     {
-        static void Main(string[] args)
+        // Singleton with Static field implementation
+        static Logger1 instance;
+        private Logger1 () { } // Private constructor - nobody can create (instantiate) an
+                              // Logger1 Object
+
+        // Property that returns the only Logger1 instance
+        public static Logger1 Instance
         {
-            Console.WriteLine("Hello World!");
+            get
+            {
+                return instance != null ? instance : new Logger1();
+            }
         }
+
+        public void LogMessage(string message)
+        {
+            Console.WriteLine("Logger 1: " + message);
+        }
+    }
+
+    public class Logger2
+    {
+        // This time we are going to use a method to return the same instance
+        static Logger2 instance;
+
+        private Logger2() { }
+
+        public static Logger2 GetInstance()
+        {
+            return instance != null ? instance : new Logger2();
+        }
+
+        public void LogMessage(string message)
+        {
+            Console.WriteLine("Logger 2: " + message);
+        }
+
     }
 }
