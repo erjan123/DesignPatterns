a78da686d1a8e8ac2465e79b63b6d1ec990c800a
erjan123 <erjan123@yahoo.com>
2018-12-13 15:30:12 +0200

    Added NUnit, XUnit and MSTest to Singleton project

---
+++ Creational Patterns/SingletonMain/SingletonMSTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SingletonMSTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
