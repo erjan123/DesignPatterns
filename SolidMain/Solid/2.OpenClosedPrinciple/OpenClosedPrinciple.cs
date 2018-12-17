7ae6048f3f092b3798f06f60f26a64f21f62155c
erjan123 <erjan123@yahoo.com>
2018-12-17 21:16:46 +0200

    Added cs files to SOLID folder

---
+++ SolidMain/Solid/2.OpenClosedPrinciple/OpenClosedPrinciple.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solid_Design_Pattern._2.OpenClosedPrinciple
+{
+    class OpenClosedPrinciple
+    {
+    }
+}
---
+++ SolidMain/Solid/4.InterfaceSegregationPrinciple/InterfaceSegregationPrinciple.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solid_Design_Pattern._4.InterfaceSegregationPrinciple
+{
+    class InterfaceSegregationPrinciple
+    {
+    }
+}
